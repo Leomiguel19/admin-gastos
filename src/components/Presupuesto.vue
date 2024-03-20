688badb8df6eba9e0b6a496ed38a018422ab792a
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-19 23:55:06 -0400

    Mostrar alerta si el presupuesto no valido

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -1,19 +1,41 @@
 <script setup>
+    import {ref} from 'vue';
+    import Alerta from "./Alerta.vue";
 
+    const presupuesto = ref(0)
+    const error = ref('')
+    
+    const definirPresupuesto = () => {
+        if(presupuesto.value <= 0){
+            console.log('Presupuesto no valido');
+            error.value = 'Presupuesto no valido';
+        }
+
+        setTimeout(() => {
+            error.value = '';
+        }, 3000);
+    }
 </script>
 
 <template>
     <form 
         class="presupuesto"
+        @submit.prevent="definirPresupuesto"
     >
+        <Alerta v-if="error">
+            {{ error }}
+        </Alerta>
+
         <div class="campo">
             <label for="nuevo-presupuesto">Definir Presupuesto</label>
 
             <input
-                type="number"
                 id="nuevo-presupuesto"
                 class="nuevo-presupuesto"
                 placeholder="Añade tu presupuesto"
+                type="number"
+                min="0"
+                v-model.number="presupuesto"
             >
         </div>
         <input type="submit" value="Definir Presupuesto">
